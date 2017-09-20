1b685f3dee1d945ceb325dd249f97b8b34350102
hm496 <hm496@163.com>
2017-09-20 10:59:25 +0800

    响应码

--- src/utils/responseCode.js
+++ src/utils/responseCode.js
@@ -1,2 +1,10 @@
 //登录成功
 export const LOGIN_SUCCESS = 110;
+/*201：用户已离线
+202：账号已停用
+204：账号不存在
+207：账号已过期*/
+export const USER_OUTLINE = 201;
+export const USER_BLOCKUP = 202;
+export const USER_NOTFOUND = 204;
+export const USER_OUTDATE = 207;

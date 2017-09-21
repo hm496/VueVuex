d263986fb759a47ca98a27b7752a4fba6df7969a
hm496 <hm496@163.com>
2017-09-21 18:25:05 +0800

    调整登录接口

--- src/routes/routerHook.js
+++ src/routes/routerHook.js
@@ -16,32 +16,32 @@ export default function (router) {
         } else {
           next();
 
-          /*          //1,显示loading页
-                    Store.commit(RootTypes.CHANGE_LOADING_MASK, true);//显示loading
-                    console.log("显示loading页");
-                    //2,去后台请求判断权限
-                    setTimeout(function () {
-                      getLoginInfo().then(function (res) {
-                        Store.commit(RootTypes.CHANGE_LOADING_MASK, false);//隐藏loading
-                        console.log("隐藏loading页");
-                        if (res.header.code === USER_OUTLINE) {
-                          //未登录
-                          Store.commit(RootTypes.CHANGE_LOGIN_STATUS, false);
-                          next("/login");
-                        } else {
-                          //已经登录
-                          //设置登录状态
-                          Store.commit(RootTypes.CHANGE_LOGIN_STATUS, true);
-                          //设置登录用户信息
-                          Store.commit(RootTypes.SET_LOGIN_INFO, res.body.info);
-                          next();
-                        }
-                      }).catch(function (err) {
-                        next("/login");
-                        Store.commit(RootTypes.CHANGE_LOADING_MASK, false);//隐藏loading
-                        console.log("隐藏loading页");
-                      });
-                    }, 300);*/
+          //1,显示loading页
+          Store.commit(RootTypes.CHANGE_LOADING_MASK, true);//显示loading
+          console.log("显示loading页");
+          //2,去后台请求判断权限
+          setTimeout(function () {
+            getLoginInfo().then(function (res) {
+              Store.commit(RootTypes.CHANGE_LOADING_MASK, false);//隐藏loading
+              console.log("隐藏loading页");
+              if (res.header.code === USER_OUTLINE) {
+                //未登录
+                Store.commit(RootTypes.CHANGE_LOGIN_STATUS, false);
+                next("/login");
+              } else {
+                //已经登录
+                //设置登录状态
+                Store.commit(RootTypes.CHANGE_LOGIN_STATUS, true);
+                //设置登录用户信息
+                Store.commit(RootTypes.SET_LOGIN_INFO, res.body.info);
+                next();
+              }
+            }).catch(function (err) {
+              next("/login");
+              Store.commit(RootTypes.CHANGE_LOADING_MASK, false);//隐藏loading
+              console.log("隐藏loading页");
+            });
+          }, 300);
         }
 
         // setTimeout(function () {

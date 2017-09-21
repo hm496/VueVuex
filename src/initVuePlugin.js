a4da77b94b453eb74fe124ae0224ac63483e0770
hm496 <hm496@163.com>
2017-09-21 15:48:05 +0800

    完善iView 配置

--- src/initVuePlugin.js
+++ src/initVuePlugin.js
@@ -3,7 +3,18 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex'
 import "./utils/vuePlugin/index";
+
+//iview相关组件
 import 'iview/dist/styles/iview.css';//iview样式
+import LoadingBar from '_iview/loading-bar';
+import Message from '_iview/message';
+import Modal from '_iview/modal';
+import Notice from '_iview/notice';
+
+Vue.prototype.$Loading = LoadingBar;
+Vue.prototype.$Message = Message;
+Vue.prototype.$Modal = Modal;
+Vue.prototype.$Notice = Notice;
 
 Vue.use(Vuex);
 Vue.use(VueRouter);

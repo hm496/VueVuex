de28c54282a98d33f9afc9b12e868766600d6dc1
hm496 <hm496@163.com>
2017-09-22 09:27:15 +0800

    修改iview 组件路径

--- src/initVuePlugin.js
+++ src/initVuePlugin.js
@@ -6,10 +6,10 @@ import "./utils/vuePlugin/index";
 
 //iview相关组件
 import 'iview/dist/styles/iview.css';//iview样式
-import LoadingBar from '_iview/loading-bar';
-import Message from '_iview/message';
-import Modal from '_iview/modal';
-import Notice from '_iview/notice';
+import LoadingBar from 'iviewSrc/loading-bar';
+import Message from 'iviewSrc/message';
+import Modal from 'iviewSrc/modal';
+import Notice from 'iviewSrc/notice';
 
 Vue.prototype.$Loading = LoadingBar;
 Vue.prototype.$Message = Message;
